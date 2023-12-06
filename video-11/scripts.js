45082661d7c4ad028d1946fd063d44dae8aed510
Rachid Touil <2339566@carrefour.cegepvicto.ca>
2023-12-06 15:03:17 -0500

    Version Finale

--- video-11/scripts.js
+++ video-11/scripts.js
@@ -3,9 +3,15 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let carteRetourne=0;
+let message=document.getElementById('message');
+let bestScore=1000;
+
+
+
 
 function flipCard() {
-  if (lockBoard) return;
+  if (lockBoard)  return;
   if (this === firstCard) return;
 
   this.classList.add('flip');
@@ -27,18 +33,23 @@ function flipCard() {
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-  isMatch ? disableCards() : unflipCards();
+  isMatch ? disableCards() : unflipCards() ;
 }
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+  // j'incremente la variable carteRetourne pour savoir si toutes les cartes son retournées
+  carteRetourne++;
+  
+  
 
   resetBoard();
 }
 
 function unflipCards() {
-  lockBoard = true;
+  lockBoard = true
+  ;
 
   setTimeout(() => {
     firstCard.classList.remove('flip');
@@ -61,3 +72,22 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+//ajout du compteur de click s'arrete si toutes les carte sont retournées
+let nbClick=0;
+function compteurClick(){
+  if(carteRetourne!=6){
+  let compteur=document.getElementById('nb-click');
+  nbClick++;
+  compteur.innerText=nbClick;
+}
+else{
+message.innerText="Bravo tu as gagné ";
+if(nbClick<bestScore)
+bestScore=nbClick;
+localStorage.setItem('bestScore',bestScore);
+}
+}
+let divBestScore=document.getElementById('bestScore');
+divBestScore.textContent+=localStorage.getItem('bestScore');
+
